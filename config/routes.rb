e2f54fe1356e14f2d2acfcd8c7368cb85dd37f9d
Anuvis Mejia <ameji012@gmail.com>
2015-03-31 17:21:42 -0400

    devise  views/controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
   HighVoltage.configure do |config|
     config.home_page = 'home'
+  end
+
+  Rails.application.routes.draw do
+      devise_for :users, controllers: {
+        sessions: 'sessions'
+      }
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
