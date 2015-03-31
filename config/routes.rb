a7af9091d0b3750053c8c7ba7760f96e44f3d398
Anuvis Mejia <ameji012@gmail.com>
2015-03-31 17:12:41 -0400

    home page initialized w/ hight colt

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  HighVoltage.configure do |config|
+    config.home_page = 'home'
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
