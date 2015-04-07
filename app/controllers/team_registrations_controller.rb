48f9da3b944fdc028bafc9239ebd92855977d9e3
Anuvis Mejia <ameji012@gmail.com>
2015-04-07 12:06:51 -0400

    basic stripe

--- app/controllers/team_registrations_controller.rb
+++ app/controllers/team_registrations_controller.rb
@@ -1,9 +1,40 @@
 class TeamRegistrationsController < InheritedResources::Base
+  attr_accessor :stripeToken
+
+  def create
+    @team_registration = TeamRegistration.new(team_registration_params)
+    respond_to do |format|
+      if @team_registration.save
+        format.html { redirect_to @team_registration, notice: 'Team Registration was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @team_registration }
+        format.js   { render :nothing => true, status: :created, location: @team_registration }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @team_registration.errors, status: :unprocessable_entity }
+        format.js   { render json: @team_registration.errors, status: :unprocessable_entity }
+      end
+    end
+    
+    Stripe.api_key = Rails.application.secrets.stripe["secret_key"]
+    token = params[:stripeToken]
+    email = params['team_registration']['captain_email']
+    begin
+      charge = Stripe::Charge.create(
+        :amount => Rails.application.secrets.stripe["registration_price"],
+        :currency => "usd",
+        :card => token,
+        :description => Rails.application.secrets.stripe["registration_description"],
+        :receipt_email => email
+      )
+    rescue Stripe::CardError => e
+      # The card has been declined
+    end
+  end
 
   private
 
     def team_registration_params
-      params.require(:team_registration).permit(:team_name, :captain_name, :captain_email, :team_members)
+      params.require(:team_registration).permit(:team_name, :captain_name, :captain_email, :team_members, :stripeToken)
     end
 end
 
