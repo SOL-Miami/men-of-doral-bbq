3c9cb76215b07ecc0f6b3d375267b71d512ab9d9
Anuvis Mejia <ameji012@gmail.com>
2015-04-07 11:10:21 -0400

    schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   resources :team_registrations
+  resources :contacts
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   
   ActiveAdmin.routes(self)
 
-  resources :contacts
+  
 
   HighVoltage.configure do |config|
     config.home_page = 'home'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150331210829) do
+ActiveRecord::Schema.define(version: 20150407150701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,15 @@ ActiveRecord::Schema.define(version: 20150331210829) do
     t.text     "message"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "team_registrations", force: :cascade do |t|
+    t.string   "team_name"
+    t.string   "captain_name"
+    t.string   "captain_email"
+    t.integer  "team_members"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "users", force: :cascade do |t|
