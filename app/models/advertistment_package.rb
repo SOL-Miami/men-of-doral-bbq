5f7a1d239126653286a30be974fbf730f5379a79
Anuvis Mejia <ameji012@gmail.com>
2015-04-23 00:52:36 -0400

    small changes

--- app/models/advertistment_package.rb
+++ app/models/advertistment_package.rb
@@ -1,6 +1,6 @@
 class AdvertistmentPackage < ActiveRecord::Base
   PACKAGES = {"Platinum/Under": Rails.application.secrets.stripe["platinum_package"], 
-            "Gold": Rails.application.secrets.stripe["gold_package"],
-            "Silver": Rails.application.secrets.stripe["silver_package"]
-           }
+              "Gold": Rails.application.secrets.stripe["gold_package"],
+              "Silver": Rails.application.secrets.stripe["silver_package"]
+             }
 end
