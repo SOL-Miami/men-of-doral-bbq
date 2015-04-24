1cb3a1725edfbde455405f174c7158ed446887ba
Mark Natera <marknatera@gmail.com>
2015-04-23 23:04:58 -0400

    CSS Edits

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,2 @@
 class Contact < ActiveRecord::Base
-  validates :name, presence: true
-  validates :email, presence: true
-  validates :phone_number, presence: true
-  validates :message, presence: true
 end
