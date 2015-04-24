b4a41f0487d322f4266c9a3569dde6584c0127b5
Anuvis Mejia <ameji012@gmail.com>
2015-04-23 20:52:56 -0400

    no message validaton

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,5 +2,4 @@ class Contact < ActiveRecord::Base
   validates :name, presence: true
   validates :email, presence: true
   validates :phone_number, presence: true
-  validates :message, presence: true
 end
