821709d5f7300a614ecff16336ea79b94412d5b6
Anuvis Mejia <ameji012@gmail.com>
2015-05-02 18:13:49 -0400

    changed email to

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,19 +3,19 @@ class ContactMailer < ApplicationMailer
   def contact_email(contact)
     @contact = contact
     @url  = 'http://bbq.whatisaman.com/'
-    mail(to: "ameji012@gmail.com", subject: 'Contact Email')
+    mail(to: "info@whatisaman.com", subject: 'Contact Email')
   end
 
   def team_registration_email(team_registration)
     @team_registration = team_registration
     @url  = 'http://bbq.whatisaman.com/'
-    mail(to: "ameji012@gmail.com", subject: 'Team Registration Email')
+    mail(to: "info@whatisaman.com", subject: 'Team Registration Email')
   end
 
   def advertisment_package_email(advertisment_package)
     @advertisment_package = advertisment_package
     @url  = 'http://bbq.whatisaman.com/'
-    mail(to: "ameji012@gmail.com", subject: 'Advertisement Package Email')
+    mail(to: "info@whatisaman.com", subject: 'Advertisement Package Email')
   end
 
 end
