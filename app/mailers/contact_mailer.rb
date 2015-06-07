0dc27aceff82130bb176cccedc7d0d87f1ab512f
Mark Natera <marknatera@gmail.com>
2015-06-07 15:15:37 -0400

    Mailer layout

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,11 @@
 class ContactMailer < ApplicationMailer
+
+  layout false
+
   def contact_email(user)
     @user = user
     @url  = 'http://bbq.whatisaman.com/'
     mail(to: "ameji012@gmail.com", subject: 'Contact Email')
   end
+  
 end
