5f3c788e3177b53450307852abe0e76a543e0816
Anuvis <anuvis@inceptures.com>
2015-06-07 19:26:53 -0400

    mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -18,4 +18,12 @@ class ContactMailer < ApplicationMailer
     mail(to: "info@whatisaman.com", subject: 'Advertisement Package Email')
   end
 
+  def advertisment_package_email(contact)
+    @contact = contact
+
+    @url  = 'http://bbq.whatisaman.com/'
+    mail(to: @contact.email, subject: 'Mens of Doral BBQ Challenge')
+  end
+
+
 end
